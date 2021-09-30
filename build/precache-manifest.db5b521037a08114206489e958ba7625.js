1699534acababf71e23386ab64991366fd5f0e48
Anurag Yadav <anuragy554@gmail.com>
2021-09-30 21:00:18 +0530

    build upload

---
+++ build/precache-manifest.db5b521037a08114206489e958ba7625.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52fded2ebec7b93ab940",
+    "url": "/static/css/main.afa33837.chunk.css"
+  },
+  {
+    "revision": "52fded2ebec7b93ab940",
+    "url": "/static/js/main.52fded2e.chunk.js"
+  },
+  {
+    "revision": "a9cf0248f65593a073d9",
+    "url": "/static/css/1.f735b5f7.chunk.css"
+  },
+  {
+    "revision": "a9cf0248f65593a073d9",
+    "url": "/static/js/1.a9cf0248.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eee3a771f95de96db9d3a30b5abf396b",
+    "url": "/static/media/banner-new-bg.eee3a771.jpg"
+  },
+  {
+    "revision": "e917e09d5952a6747d36e55313eeb68e",
+    "url": "/index.html"
+  }
+];
